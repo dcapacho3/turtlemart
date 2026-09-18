863019fb7a2518e25ab30be50a318ef1dcaa9090
dcapacho3 <dcapacho3@gmail.com>
2026-09-17 23:25:49 -0500

    Remove build artifacts and caches from version control

--- build/turtlemart/CMakeFiles/turtlemart_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/turtlemart_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/turtlemart_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
